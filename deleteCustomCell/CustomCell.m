65a40dbfdfba52c537d3d2aab4e2af3af8465199
admin <admin@MacBook-admin.local>
2013-05-28 21:57:38 +0300

    Learn UITableView
    Simple custom cell in tableView

---
+++ deleteCustomCell/CustomCell.m
@@ -0,0 +1,29 @@
+//
+//  CustomCell.m
+//  deleteCustomCell
+//
+//  Created by admin on 21.05.13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import "CustomCell.h"
+
+@implementation CustomCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
