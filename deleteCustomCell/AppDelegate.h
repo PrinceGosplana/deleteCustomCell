7d99b4c7488cbad346c45ceddb283c9ed6d96571
admin <admin@MacBook-admin.local>
2013-05-28 22:01:28 +0300

    Revert "Revert "Learn UITableView""
    This reverts commit 9d01ccf760f16a565a7e747bb34c8d2d03c95e54.

---
+++ deleteCustomCell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  deleteCustomCell
+//
+//  Created by admin on 21.05.13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
